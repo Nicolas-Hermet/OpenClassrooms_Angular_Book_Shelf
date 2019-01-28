d7babf02f721f7a746a13ec6507b8a0802d40dce
Nicolas Hermet <n.hermet@gmail.com>
2019-01-28 12:00:47 +0100

    configure api key for firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-book-shelf';
+  private env = require('../../../env.json');
+
+  constructor () {
+    const config = {
+      apiKey: this.env.apiKey,
+      authDomain: this.env.authDomain,
+      databaseURL: this.env.databaseURL,
+      projectId: this.env.projectId,
+      storageBucket: this.env.storageBucket,
+      messagingSenderId: this.env.messagingSenderId
+    };
+    firebase.initializeApp(config);
+  }
 }
