e1bc96e5332eda8fffda7cf5bc64f357e28e9e15
Nicolas Hermet <n.hermet@gmail.com>
2019-01-29 15:53:27 +0100

    Revert "Plug Firebase to Auth Service" revert for branch change reasons.... This reverts commit c411b6ff7c716f003344753c3235e123de398af2.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import * as firebase from 'firebase';
-import { resolve } from 'url';
-import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,38 +6,4 @@ import { reject } from 'q';
 export class AuthService {
 
   constructor() { }
-
-  createNewUser(email: string, password: string) {
-    return new Promise(
-      (resolve, reject) => {
-        firebase.auth().createUserWithEmailAndPassword(email, password).then(
-          () => {
-            resolve();
-          },
-          (error) => {
-            reject(error);
-          }
-        );
-      }
-    );
-  }
-
-  signInUser(email: string, password: string) {
-    return new Promise(
-      (resolve, reject) => {
-        firebase.auth().signInWithEmailAndPassword(email, password).then(
-          () => {
-            resolve();
-          },
-          (error) => {
-            reject(error);
-          }
-        );
-      }
-    );
-  }
-
-  signOut() {
-    firebase.auth().signOut();
-  }
 }
