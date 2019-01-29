9794b7f9cf305e05aaa4bd87f860fbab50ccbe2f
Nicolas Hermet <n.hermet@gmail.com>
2019-01-29 17:14:31 +0100

    Create array of books and server exchanges

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
+  private books: Book[] = [];
+  bookSubject = new Subject<Book[]>();
+
   constructor() { }
+
+  emitBooks() {
+    this.bookSubject.next(this.books.slice());
+  }
+
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books')
+    .on('value', (data) => {
+      this.books = data.val() ? data.val() : [];
+      this.emitBooks();
+    });
+  }
 }
