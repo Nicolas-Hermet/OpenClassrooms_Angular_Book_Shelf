f1942c849c1af417afbd09e1f62ed1aeaafcbc02
Nicolas Hermet <n.hermet@gmail.com>
2019-01-30 10:22:41 +0100

    Add book list management

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Book } from '../models/book.model';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  bookSubscritpion: Subscription;
+
+  constructor(private booksService: BooksService, private router: Router) { }
 
   ngOnInit() {
+    this.bookSubscritpion = this.booksService.bookSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
   }
 
+    onNewBook() {
+      this.router.navigate(['/books', 'new']);
+    }
+
+    onDeleteBook(book: Book) {
+      this.booksService.removeBook(book);
+    }
+
+    onViewBook(id: number) {
+      this.router.navigate(['/books', 'view', id]);
+    }
+
+    ngOnDestroy(): void {
+      // Called once, before the instance is destroyed.
+      // Add 'implements OnDestroy' to the class.
+      this.bookSubscritpion.unsubscribe();
+    }
 }
